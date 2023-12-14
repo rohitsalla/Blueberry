dffc78d1e168b75d08ec10c3305352b4d4df44f5
rohitsalla <sallarohit1011@gmail.com>
2023-12-14 19:20:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 mkdir -p ~/.streamlit/
+echo [$(date)]: "START"
+echo [$(date)]: "creating env with python 3.8 version" 
+conda create --prefix ./env python=3.8 -y
+echo [$(date)]: "activating the environment" 
+source activate ./env
+echo [$(date)]: "installing the dev requirements" 
+pip install -r requirements.txt
 echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
